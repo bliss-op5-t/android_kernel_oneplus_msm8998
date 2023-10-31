45a6d5aa54c4aad1a6b03045c48056628e57d68b
Jaegeuk Kim <jaegeuk@kernel.org>
2023-10-31 20:19:15 +0800

    f2fs: prepare a waiter before entering io_schedule
    This is to avoid sleep() in the waiter thread.
    
    [   20.157753] ------------[ cut here ]------------
    [   20.159858] WARNING: CPU: 1 PID: 1152 at kernel/sched/core.c:7142 __might_sleep+0x149/0x1a0
    ...
    [   20.176110]  __submit_merged_write_cond+0x191/0x310
    [   20.176739]  f2fs_submit_merged_write+0x18/0x20
    [   20.177323]  f2fs_wait_on_all_pages+0x269/0x2d0
    [   20.177899]  ? block_operations+0x980/0x980
    [   20.178441]  ? __kasan_check_read+0x11/0x20
    [   20.178975]  ? finish_wait+0x260/0x260
    [   20.179488]  ? percpu_counter_set+0x147/0x230
    [   20.180049]  do_checkpoint+0x1757/0x2a50
    [   20.180558]  f2fs_write_checkpoint+0x840/0xaf0
    [   20.181126]  f2fs_sync_fs+0x287/0x4a0.
    
    Reported-by: Eric Biggers <ebiggers@kernel.org>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Change-Id: I727ff7192e5859f55e06547b8334b5728cd78a55.

--- fs/f2fs/checkpoint.c
+++ fs/f2fs/checkpoint.c
@@ -1259,8 +1259,6 @@ void f2fs_wait_on_all_pages(struct f2fs_sb_info *sbi, int type)
 	DEFINE_WAIT(wait);
 
 	for (;;) {
-		prepare_to_wait(&sbi->cp_wait, &wait, TASK_UNINTERRUPTIBLE);
-
 		if (!get_pages(sbi, type))
 			break;
 
@@ -1271,7 +1269,9 @@ void f2fs_wait_on_all_pages(struct f2fs_sb_info *sbi, int type)
 			f2fs_sync_meta_pages(sbi, META, LONG_MAX,
 							FS_CP_META_IO);
 		else if (type == F2FS_WB_CP_DATA)
-			f2fs_submit_merged_write(sbi, DATA);					
+			f2fs_submit_merged_write(sbi, DATA);
+
+		prepare_to_wait(&sbi->cp_wait, &wait, TASK_UNINTERRUPTIBLE);
 		io_schedule_timeout(HZ/50);
 	}
 	finish_wait(&sbi->cp_wait, &wait);
